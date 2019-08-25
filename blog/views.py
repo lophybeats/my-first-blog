29cbda890c900479866ed3a1b9a8561967b81964
lophybeats <lophybeats@gmail.com>
2019-08-25 19:16:44 +0530

    l

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    context = {'posts': posts}
-    return render(request, 'blog/post_list.html', context)
+    return render(request, 'blog/post_list.html', {'posts': posts})
